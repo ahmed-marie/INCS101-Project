8fba637110795a9a2fb8e32b9927ad8ef4b5b76a
Ahmed Marie <ahmedkamelmarie@gmail.com>
2024-12-14 11:50:25 +0200

    Developing Deck Class
    Developing the Deck class' methods

--- Project/Deck.cpp
+++ Project/Deck.cpp
@@ -38,23 +38,36 @@ Deck::Deck()
 		// 2. initialize the second card in the pair
 		cardsArr[arrayIndex++] = new PenaltyCard(counterIndex, false);
 	}
+	// initialized the number of removed cards to 0
+	removedCards = 0;
+
+	// initialize the revealedCardsIndex Array elements to -1
+	revealedCardsIndex[0] = -1;
+	revealedCardsIndex[1] = -1;
 }
 
 
 Deck::Deck(Card** cardsArr)
 {
 	this->cardsArr = cardsArr;
+
+	// initialized the number of removed cards to 0
+	removedCards = 0;
+
+	// initialize the revealedCardsIndex Array elements to -1
+	revealedCardsIndex[0] = -1;
+	revealedCardsIndex[1] = -1;
 }
 
 
 Deck::~Deck()
 {
-
 	// delete the dynamically-created 1D array of Card* cardsArr
 	// first delete each dynamically allocated entry (pointed by each pointer)
 	for (int i = 0; i < DECK_SIZE; i++)
 	{
-		delete cardsArr[i];
+		if(cardsArr[i] != nullptr)
+			delete cardsArr[i];
 	}
 	// delete the whole array
 	delete[] cardsArr;
@@ -91,9 +104,184 @@ void Deck::displayGrid()
 		for (int j = 0; j < GRID_SIZE; j++)
 		{
 			int index = i * GRID_SIZE + j; // i * j - 1
-			cardsArr[index]->display();
+			// if the card pointer is not a nullptr 
+			// (not removed from array)
+			if (cardsArr[index] != nullptr)
+			{
+				cardsArr[index]->display();
+			}
+			else
+			{
+				cout << " ";
+			}
 			cout << " ";
 		}
 		cout << endl;
 	}
+}
+
+bool Deck::isCardExists(int index)
+{
+	return cardsArr[index] != nullptr;
+}
+
+DECK_STATUS_T Deck::getDeckStatus()
+{
+	if (removedCards == DECK_SIZE)
+	{
+		return EMPTY_DECK;
+	}
+	else if (removedCards == (DECK_SIZE - 1))
+	{
+		return ONE_CARD_LEFT;
+	}
+	else
+	{
+		return TWO_OR_MORE_CARDS_LEFT;
+	}
+}
+
+CARD_EVENT_T  Deck::revealCard(int row, int col)
+{
+	// converts row and col to index. 
+	// row and col have values between 1 and GRID_SIZE inclusive
+	int index = (row - 1) * GRID_SIZE + col - 1; // i * j - 1
+	if (isCardExists(index) == false)
+	{
+		return CARD_NOT_FOUND;
+	}
+	else
+	{
+		// Flip the card
+		cardsArr[index]->setFaceUp(true);
+		// add its index in the reavealedCardIndex
+		// 0 or 1 whether 0 is free
+		if (revealedCardsIndex[0] == -1)
+		{
+			revealedCardsIndex[0] = index;
+		}
+		else
+		{
+			revealedCardsIndex[1] = index;
+		}
+		// display the card's message
+		cardsArr[index]->displayCardMessage();
+		
+		return CARD_FOUND;
+	}
+}
+
+REVEALED_CARDS_EVENT_T Deck::evaluateFlippedCards()
+{
+	REVEALED_CARDS_EVENT_T status;
+	// get the card pointers
+	Card* card1_ptr = cardsArr[revealedCardsIndex[0]];
+	Card* card2_ptr = cardsArr[revealedCardsIndex[1]];
+	// get their corresponding type
+	CARD_TYPE_T card1_type = card1_ptr->getCardType();
+	CARD_TYPE_T card2_type = card2_ptr->getCardType();
+
+	// Determine the card's status 
+	// (numbered according to cases in Notion)
+
+	// two standard cards: 1. same 7. different
+	if ((card1_type == STANDARD) && (card2_type == STANDARD))
+	{
+		if (card1_ptr->getNumber() == card1_ptr->getNumber())
+		{
+			status = TWO_SAME_STANDARD;
+		}
+		else
+		{
+			status = TWO_DIFFERENT_STANDARD;
+		}
+	}
+	// 2. Two penalty cards
+	else if ((card1_type == PENALTY) && (card2_type == PENALTY))
+	{
+		status = TWO_PENALTY;
+	}
+	// 3. Two bonus cards
+	else if ((card1_type == BONUS) && (card2_type == BONUS))
+	{
+		status = TWO_BONUS;
+	}
+	// 4. A Bonus card and a Penalty Card
+	else if (((card1_type == BONUS) && (card2_type == PENALTY)) ||
+		((card1_type == PENALTY) && (card2_type == BONUS)))
+	{
+		status = BONUS_AND_PENALTY;
+	}
+	// 5. A Standard card and a Bonus Card
+	else if (((card1_type == STANDARD) && (card2_type == BONUS)) ||
+		((card1_type == BONUS) && (card2_type == STANDARD)))
+	{
+		status = STANDARD_AND_BONUS;
+	}
+	// 6. A Standard card and a Penalty Card
+	else if (((card1_type == STANDARD) && (card2_type == PENALTY)) ||
+		((card1_type == PENALTY) && (card2_type == STANDARD)))
+	{
+		status = STANDARD_AND_PENALTY;
+	}
+	else
+	{
+		// non existing case
+	}
+	
+	// manipulating deck and cards based on their type
+	switch (status)
+	{
+		// cases 1. to 4.
+		case TWO_SAME_STANDARD:
+		case TWO_PENALTY:
+		case TWO_BONUS:
+		case BONUS_AND_PENALTY:
+			// delete both cards
+			delete card1_ptr;
+			delete card2_ptr;
+			// set the pointers to nullptr
+			cardsArr[revealedCardsIndex[0]] = nullptr;
+			cardsArr[revealedCardsIndex[1]] = nullptr;
+			// increment removedCards by 2
+			removedCards += 2;
+			break;
+
+		// case 5. and 6.
+		case STANDARD_AND_BONUS:
+		case STANDARD_AND_PENALTY:
+			if (card1_type == STANDARD)
+			{
+				// flip the standard card
+				cardsArr[revealedCardsIndex[0]]->setFaceUp(false);
+				// delete the bonus/penalty card
+				delete card2_ptr;
+				cardsArr[revealedCardsIndex[1]] = nullptr;
+			}
+			else
+			{
+				// flip the standard card
+				cardsArr[revealedCardsIndex[1]]->setFaceUp(false);
+				// delete the bonus/penalty card
+				delete card1_ptr;
+				cardsArr[revealedCardsIndex[0]] = nullptr;
+			}
+			// increment removedCards by 1
+			removedCards++;
+			break;
+
+		case TWO_DIFFERENT_STANDARD:
+			// flip the standard cards
+			cardsArr[revealedCardsIndex[0]]->setFaceUp(false);
+			cardsArr[revealedCardsIndex[1]]->setFaceUp(false);
+
+		default: break;
+	}
+
+	// set the card indices in revealedCardsIndex to -1
+	revealedCardsIndex[0] = -1;
+	revealedCardsIndex[1] = -1;
+
+	// return the status
+	return status;
 }
--- Project/Deck.h
+++ Project/Deck.h
@@ -17,13 +17,35 @@
                    NUMBER_OF_BONUS_CARDS_PAIRS + \
                    NUMBER_OF_PENALTY_CARDS_PAIRS))
 
+typedef enum CARD_EVENT
+{
+	CARD_NOT_FOUND, CARD_FOUND
+} CARD_EVENT_T;
+
+typedef enum REVEALED_CARDS_EVENT
+{
+	TWO_SAME_STANDARD, TWO_DIFFERENT_STANDARD,
+	TWO_PENALTY, TWO_BONUS, STANDARD_AND_BONUS,
+	STANDARD_AND_PENALTY, BONUS_AND_PENALTY
+} REVEALED_CARDS_EVENT_T;
+
+typedef enum DECK_STATUS
+{
+	EMPTY_DECK, ONE_CARD_LEFT, TWO_OR_MORE_CARDS_LEFT
+} DECK_STATUS_T;
+
 class Deck
 {
 private:
 	// cardsArr represents the array of cards (DECK_SIZE)
 	// 6 pairs of StandardCard, 1 PenaltyCard and 1 BonusCard
 	Card** cardsArr;
-
+	// removedCards represented the number
+	// of removed cards from the deck
+	int removedCards;
+	// revealedCardsIndex represents the
+	// indicies of the revealed cards
+	int revealedCardsIndex[2];
 public:
 	Deck();
 	Deck(Card** cardsArr); // not tested
@@ -32,6 +54,11 @@ public:
 	Card** getCardsArr();
 	void shuffle();
 	void displayGrid();
+	bool isCardExists(int index);
+	DECK_STATUS_T getDeckStatus();
+	CARD_EVENT_T revealCard(int row, int col);
+	REVEALED_CARDS_EVENT_T evaluateFlippedCards();
+
 };
 #endif
 
